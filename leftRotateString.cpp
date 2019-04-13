efb7738e41155d22e7a54cbb18c5453324e86076
root <root@hxz.com>
2019-04-13 21:04:01 +0800

    左旋转字符串（书上思路）：反转字符串

---
+++ leftRotateString.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+
+void swap(char* str1,char* str2){
+  if(str1 == NULL || str2 == NULL)
+    return;
+  char temp = *str1;
+  *str1 = *str2;
+  *str2 = temp;
+}
+
+
+void reverseString(char* str){
+  if(str == NULL || str[0] == '\0')
+    return;
+  int right = strlen(str) - 1;
+  int left = 0;
+  while(left < right){
+    swap(&str[left], &str[right]);
+    ++ left;
+    -- right;
+  }
+}
+
+int main(){
+  char str[20] = "Hello world!";
+  int l = strlen(str);
+  std::cout << "length:" << l << std::endl;
+
+  return 0;
+}
